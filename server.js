9f74d1c9aa962f0c93a120655904a4ed6c86d3f5
黃丞廷 <blacknight@huangchengtingde-MacBook-Pro.local>
2017-02-11 17:54:20 +0800

    Modify server.js

--- server.js
+++ server.js
@@ -14,16 +14,17 @@ var app = express();
 var server = http.createServer(app);
 
 function EchoFunc(req,res){
+  res.writeHead(200, {'Content-Type': 'text/plain ;charset=utf-8;'});
   res.end("(´・ω・｀)!"+req.url)
 };
 
 function HelloWorld(req,res){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end("Hello world");
+  res.writeHead(200, {'Content-Type': 'text/plain ;charset=utf-8;'});
+  res.end("Hello world");
 };
 
 app.get('/',HelloWorld);
-app.get('/echo/',EchoFunc);
+app.get('/echo',EchoFunc);
 
 server.listen(3000, '127.0.0.1',function(){
   console.log("3(´・ω・｀)!")
